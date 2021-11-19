e821f63680b057dd632d2ac1e886fd8c70651a18
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-11-19 18:46:57 -0300

    :white_check_mark: "Test: Component tests About"

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testa o componente "About"', () => {
+  it('Se possui o título "About Pokedéx"', () => {
+    render(<About />);
+    const titleElement = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it('Se possui 2 parágrafos sobre Pokémons', () => {
+    render(<About />);
+    const paragraphs = screen.getAllByText(/pokémons/i);
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  it('Se possui a imagem de uma Pokédex', () => {
+    render(<About />);
+    const pokedexImg = screen.getByAltText('Pokédex');
+    const imgUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(pokedexImg.src).toBe(imgUrl);
+  });
+});
