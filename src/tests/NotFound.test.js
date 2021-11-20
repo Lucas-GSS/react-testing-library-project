e677407c36520bcba409a7b9fb882d1230345658
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-11-19 22:14:47 -0300

    :white_check_mark: "Test: Not Found component test"

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa a rota de "NotFound"', () => {
+  it('Ao receber uma url inexistente o componente NotFound é renderizado', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/nãoexiste');
+    const titleNotFound = screen.getByRole('heading', {
+      name: /page requested not found/i });
+    const imgNotFound = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found' });
+    expect(titleNotFound).toBeInTheDocument();
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
